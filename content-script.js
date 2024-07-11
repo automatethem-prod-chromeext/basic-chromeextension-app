68fa6fdc6bfec8c7793e5e1398b333d30840d69c
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-07-11 12:52:35 +0900

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,14 +1,3 @@
 console.log("I am content-script.js");
 
 document.body.style.backgroundColor = 'blue';
-
-//메시지 받기
-
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.type == 'from popup.js')
-    document.body.style.backgroundColor = 'green';
-  console.log('content-script.js received message');
-  console.log(message);
-  sendResponse({ message: 'content-script.js received message', receivedMessage: message });
-  return true;
-});
