47abc705392ce5f53e14b64a8e1ce62de4873577
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-07-11 12:52:18 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,33 +5,5 @@ console.log("I am popup.js");
   //const button = document.getElementById("button");
   button.addEventListener("click", async () => {
     document.body.style.backgroundColor = 'red';
-    //background.js 로 메시지 보내기 시작
-    const response1 = await chrome.runtime.sendMessage({
-      type: 'from popup.js',
-      payload: {
-          message: 'from popup.js',
-      },
-    });
-    //console.log(response1);
-    //background.js 로 메시지 보내기 끝
-    //content-script.js 로 메시지 보내기 시작
-    const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-    const tab = tabs[0];
-    const response2 = await chrome.tabs.sendMessage(tab.id, {
-      type: 'from popup.js',
-      payload: {
-          message: 'from popup.js',
-      },
-    });
-    //const response2 = await chrome.scripting.executeScript({
-    //  target: { tabId: tab.id },
-    //  args: [],
-    //  function: function () {
-    //    document.body.style.backgroundColor = 'green';
-    //    return null;
-    //  }
-    //});
-    //console.log(response2);
-    //content-script.js 로 메시지 보내기 끝
   });
 //});
