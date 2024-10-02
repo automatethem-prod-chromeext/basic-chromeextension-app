5cd2cd76b7037c701a82aae4daa8d08ef2f3783d
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-10-02 22:07:08 +0900

    Update background.js

--- background.js
+++ background.js
@@ -3,9 +3,11 @@ console.log("I am background.js")
 chrome.runtime.onInstalled.addListener(async (details) => {
   // 확장 프로그램이 설치 혹은 업데이트될 때 특정 URL로 이동
   if (details.reason === 'install' || details.reason === 'update') {
-    //chrome.tabs.create({ url: "https://sendtoai.automatethem.co/install.html" });
+    chrome.tabs.create({ url: "https://sendtoai.automatethem.co/install.html" });
+    //chrome.tabs.create({ url: "https://google-chrome-extensions.com/apps/youtube-transcription/welcome/" });
   }
 });
 
 // 확장 프로그램이 제거될 때 특정 URL로 이동
-//chrome.runtime.setUninstallURL("https://sendtoai.automatethem.co/uninstall.html");
+chrome.runtime.setUninstallURL("https://sendtoai.automatethem.co/uninstall.html");
+//chrome.runtime.setUninstallURL("https://google-chrome-extensions.com/apps/youtube-transcription/help-us/");
